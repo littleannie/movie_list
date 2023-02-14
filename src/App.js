cbbd878bb59ce7cce698286d18dafdfeb3e79a34
Anne Ly <anne.ly59@gmail.com>
2023-02-14 18:03:30 +0100

    movie-list

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
-import React from 'react';
+import { useEffect } from 'react';
+import './App.css';
+import SearchIcon from './search.svg';
+
+// c19b1da6
+
+const API_URL = "http://www.omdbapi.com?apikey=c19b1da6"
 
 const App = () => {
+
+  const SearchMovie = async (title) => {
+    const response = await fetch (`${API_URL}&s=${title}`)
+    const data = await response.json()
+
+    console.log(data.Search)
+  }
+
+  useEffect(() => {
+    SearchMovie('Spiderman')
+  }, []);
+
   return (
-    <h1>App</h1>
+    <div className="app">
+      <h1>MovieLand</h1>
+    </div>
   )
 }
 
